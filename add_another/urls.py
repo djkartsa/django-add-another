223960901018d0fd937d5bab6ecd2bd5fa93035a
Karim <djkartsa@gmail.com>
2016-02-17 01:06:11 +0200

    Cosmetic fixes.

--- add_another/urls.py
+++ add_another/urls.py
@@ -6,5 +6,3 @@ urlpatterns = patterns(
     url(r'^add/(?P<model_name>\w+)/?$', 'views.add_new_model', name='add_another'),
     url(r'^success/$', 'views.add_another_success', name='add_another_success'),
 )
-
-
